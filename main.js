585090e46be78287e687bbdf684c78b2c27abc79
Eugen <epopescuu@yahoo.it>
2023-11-15 10:45:38 +0100

    feat: add render function

--- main.js
+++ main.js
@@ -56,66 +56,56 @@ const posts = [
     }
 ];
 
-
 // svuoto il contenuto ad ogni ricarica dell pagina
 document.querySelector('#container').innerHTML = '';
 
 // ciclo foreach tutti i post
 posts.forEach((item) => {
 
-    // ho salvato in una variabile il contenuto del'html
-    let post = `
+    // ho aggiunto al container nell'html la mia variabile con il contenuto
+    // += la funzione che mi renderizza il mio post
+    document.querySelector('#container').innerHTML += renderPost(item);
+
+});
+
+// funziona che renderizza il mio post
+// item è la variabile che gli do io (puo essere diversa)
+function renderPost(item) {
+    // destrutturo do posts
+    let { id, content, media, author, likes, created } = item;
+    let { name, image } = author;
+
+    let result = `
     <div class="post">
         <div class="post__header">
             <div class="post-meta">                    
                 <div class="post-meta__icon">
-                    <img class="profile-pic" src="${item.author.image}" alt="${item.author.name}">                    
+                    <img class="profile-pic" src="${image}" alt="${name}">                    
                 </div>
                 <div class="post-meta__data">
-                    <div class="post-meta__author">${item.author.name}</div>
-                    <div class="post-meta__time">${item.created}</div>
+                    <div class="post-meta__author">${name}</div>
+                    <div class="post-meta__time">${created}</div>
                 </div>                    
             </div>
         </div>
-        <div class="post__text">${item.content}</div>
+        <div class="post__text">${content}</div>
         <div class="post__image">
-            <img src="${item.media}" alt="">
+            <img src="${media}" alt="">
         </div>
         <div class="post__footer">
             <div class="likes js-likes">
                 <div class="likes__cta">
-                    <a class="like-button  js-like-button" href="#" data-postid="1">
+                    <a class="like-button  js-like-button" href="#" data-postid="${id}">
                         <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-                        <span class="like-button__label">${item.likes}</span>
+                        <span class="like-button__label">${likes}</span>
                     </a>
                 </div>
                 <div class="likes__counter">
-                    Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
+                    Piace a <b id="like-counter-${id}" class="js-likes-counter">80</b> persone
                 </div>
             </div> 
         </div>            
     </div>`;
-    
-    // ho aggiunto al container nell'html la mia variabile con il contenuto
-    document.querySelector('#container').innerHTML += post;
 
-    // like button 
-    document.querySelector('.like-button').addEventListener('click', function () {
-        let button = document.querySelector('.like-button');
-
-        if (button.classList.contains('.like-button__label')) {
-            
-            button.classList.remove('.like-button__label');
-            button.classList.add('.like-button--liked');
-        } else {
-            button.classList.remove('.like-button--liked');
-            button.classList.add('.like-button__label');
-        }
-    })
-
-    // bonus 4
-    if(item.author.image === '') {
-        item.author.image = '17004.png';
-    }
-
-});
+    return result;
+}
